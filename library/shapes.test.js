2ee756dbf6fac3d5953ff98f367e0f5379849b6a
D3C0D1NG <joseph.s.foster@icloud.com>
2023-08-30 20:42:08 -0500

    added tests for each shape

--- library/shapes.test.js
+++ library/shapes.test.js
@@ -6,3 +6,17 @@ test("Should render green circle.", () => {
     expect(shape.render()).toEqual(`<circle cx="150" cy="100" r="80" fill="green" />`);
 }
 );
+
+test("Should render green triangle.", () => {
+    const shape = new Triangle();
+    shape.setColor("green");
+    expect(shape.render()).toEqual(`<polygon points="150, 18 244, 182 56, 182" fill="green" />`);
+}
+);
+
+test("Should render green Square.", () => {
+    const shape = new Square();
+    shape.setColor("green");
+    expect(shape.render()).toEqual(`<rect x="90" y="40" width="120" height="120" fill="green" />`);
+}
+);
